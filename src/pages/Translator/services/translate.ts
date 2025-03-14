29d0052793205e0a85f8ae0b70f654509ddb10ff
axeltrejodev <axeltrejo.dev@gmail.com>
2025-03-14 12:06:06 -0600

    OpenAI API key update

--- src/pages/Translator/services/translate.ts
+++ src/pages/Translator/services/translate.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey:
-    "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",
+    "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",
   dangerouslyAllowBrowser: true,
 });
 
