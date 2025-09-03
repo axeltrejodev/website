0c0d850f7455a5afe57da3c9ed3c3fdf900e3c4c
axeltrejodev <axeltrejo.dev@gmail.com>
2025-09-03 18:00:00 +0000

    feat: add confetti effect to easter egg

--- src/components/main/konami.tsx
+++ src/components/main/konami.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import confetti, { type Shape } from "canvas-confetti";
 import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 
 import { toast } from "@/uva/toaster";
+
+import { PROJECT_EMOJIS } from "@/lib/setup";
 
 const konamiCode = [
   "ArrowUp",
@@ -14,19 +17,53 @@ const konamiCode = [
   "ArrowRight",
   "ArrowLeft",
   "ArrowRight",
-  "b",
-  "a",
+  "B",
+  "A",
 ];
+
+const flatKonami = konamiCode.join("").toLowerCase();
 
 export function Konami() {
   const t = useTranslations("utils");
   const [, setInput] = useState<string[]>([]);
+  const [emojis, setEmojis] = useState<Shape[]>([]);
+  useEffect(
+    () =>
+      setEmojis(
+        [
+          "👽", //
+          "🤍",
+          "💙",
+          "🦀",
+          ...PROJECT_EMOJIS,
+        ].map((emoji) =>
+          confetti //
+            .shapeFromText({
+              text: emoji,
+              scalar: 2.5,
+            }),
+        ),
+      ),
+    [],
+  );
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       setInput((prev) => {
         const newInput = [...prev, e.key].slice(-konamiCode.length);
-        if (newInput.join("") === konamiCode.join("")) {
+        if (newInput.join("").toLowerCase() === flatKonami) {
           toast(t("easterEgg"));
+          confetti({
+            shapes: emojis,
+            angle: 90,
+            ticks: 720,
+            scalar: 2.5,
+            spread: 180,
+            decay: 0.933,
+            gravity: 0.5,
+            origin: { y: 1 },
+            particleCount: 60,
+            startVelocity: 60,
+          });
           return [];
         }
         return newInput;
@@ -34,6 +71,6 @@ export function Konami() {
     };
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [t]);
+  }, [t, emojis]);
   return null;
 }
